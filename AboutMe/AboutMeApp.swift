ebb046e3c0eb57711b085bb619fdf7753af413ab
Lily Mosisa <lilymosisa13@gmail.com>
2025-04-13 17:00:36 -0400

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Lily Mosisa on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
